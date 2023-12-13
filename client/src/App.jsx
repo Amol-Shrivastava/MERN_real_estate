186aa0ae10d040ef49727853a39657d456a7d730
amol <amolshrivastava448@gmail.com>
2023-12-14 00:11:58 +0530

    install react js and tailwind

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+ console.log('hello');
+  return (
+    <div>
+      <h1 className="text-red-600">App</h1>
+    </div>
+  )
+}
+
+export default App
