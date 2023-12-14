3c3d4e4aca2c2ab117ba8414d667ddbc32425280
amol <amolshrivastava448@gmail.com>
2023-12-14 23:00:35 +0530

    connected to MONGODB

--- server/index.js
+++ server/index.js
@@ -1,7 +1,27 @@
 import express from "express";
-
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
 
-app.listen(3000, () => {
-  console.log("server is running 3000 port");
-});
+const startDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_DB_URI);
+    console.log("Connected To DB");
+  } catch (error) {
+    throw error;
+  }
+};
+
+const startServer = async () => {
+  try {
+    await startDB();
+    app.listen(3000, () => {
+      console.log("server is running 3000 port");
+    });
+  } catch (error) {
+    throw error;
+  }
+};
+
+startServer();
